fcfe1c3268c4b1100669aa47e4292862c44bd5ed
Loren Gordon <lorengordon@users.noreply.github.com>
2019-07-05 06:45:17 -0500

    Adds support for tf 0.12 and runs upgrade tool

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
 data "aws_iam_policy" "cloudwatch_readonly" {
   arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
 }
 
 module "lambda" {
-  source = "git::https://github.com/plus3it/terraform-aws-lambda.git?ref=v0.12.0"
+  source = "git::https://github.com/plus3it/terraform-aws-lambda.git?ref=v1.1.0"
 
-  function_name = "${var.name}"
+  function_name = var.name
   description   = "Post messages from AWS to Slack"
   handler       = "aws-to-slack/src/index.handler"
   runtime       = "nodejs8.10"
@@ -14,58 +18,66 @@ module "lambda" {
   source_path = "${path.module}/node_modules"
 
   attach_policy = "true"
-  policy        = "${data.aws_iam_policy.cloudwatch_readonly.policy}"
+  policy        = data.aws_iam_policy.cloudwatch_readonly.policy
 
   environment = {
     variables = {
-      SLACK_HOOK_URL = "${var.hook_url}"
+      SLACK_HOOK_URL = var.hook_url
     }
   }
 }
 
 resource "aws_cloudwatch_event_rule" "this" {
-  count = "${length(var.event_rules)}"
+  count = length(var.event_rules)
 
-  name          = "${lookup(var.event_rules[count.index], "name", "${var.name}_${count.index}")}"
-  description   = "${lookup(var.event_rules[count.index], "description", "Managed by Terraform")}"
-  event_pattern = "${lookup(var.event_rules[count.index], "pattern")}"
+  name = lookup(
+    var.event_rules[count.index],
+    "name",
+    "${var.name}_${count.index}",
+  )
+  description = lookup(
+    var.event_rules[count.index],
+    "description",
+    "Managed by Terraform",
+  )
+  event_pattern = var.event_rules[count.index]["pattern"]
 }
 
 resource "aws_cloudwatch_event_target" "this" {
-  count = "${length(var.event_rules)}"
+  count = length(var.event_rules)
 
-  rule = "${aws_cloudwatch_event_rule.this.*.name[count.index]}"
-  arn  = "${module.lambda.function_arn}"
+  rule = aws_cloudwatch_event_rule.this[count.index].name
+  arn  = module.lambda.function_arn
 }
 
 resource "aws_lambda_permission" "lambda" {
-  count = "${length(var.event_rules)}"
+  count = length(var.event_rules)
 
   action        = "lambda:InvokeFunction"
-  function_name = "${module.lambda.function_name}"
+  function_name = module.lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.this.*.arn[count.index]}"
+  source_arn    = aws_cloudwatch_event_rule.this[count.index].arn
 }
 
 resource "aws_sns_topic" "this" {
-  count = "${var.sns_trigger ? 1 : 0}"
+  count = var.sns_trigger ? 1 : 0
 
-  name = "${var.name}"
+  name = var.name
 }
 
 resource "aws_sns_topic_subscription" "this" {
-  count = "${var.sns_trigger ? 1 : 0}"
+  count = var.sns_trigger ? 1 : 0
 
-  topic_arn = "${aws_sns_topic.this.arn}"
+  topic_arn = aws_sns_topic.this[0].arn
   protocol  = "lambda"
-  endpoint  = "${module.lambda.function_arn}"
+  endpoint  = module.lambda.function_arn
 }
 
 resource "aws_lambda_permission" "sns" {
-  count = "${var.sns_trigger ? 1 : 0}"
+  count = var.sns_trigger ? 1 : 0
 
   action        = "lambda:InvokeFunction"
-  function_name = "${module.lambda.function_name}"
+  function_name = module.lambda.function_name
   principal     = "sns.amazonaws.com"
-  source_arn    = "${aws_sns_topic.this.arn}"
+  source_arn    = aws_sns_topic.this[0].arn
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "hook_url" {
   description = "Slack webhook URL; see <https://api.slack.com/incoming-webhooks>"
-  type        = "string"
+  type        = string
 }
 
 variable "name" {
   default     = "aws-to-slack"
   description = "(Optional) Name to associate with the lambda function"
-  type        = "string"
+  type        = string
 }
 
 variable "event_rules" {
   description = "(Optional) List of config maps of CloudWatch Event Rules that will trigger notifications"
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "sns_trigger" {
   description = "(Optional) Toggle to control whether to create an SNS topic and subscription for the lambda function"
-  type        = "string"
+  type        = bool
   default     = false
 }
